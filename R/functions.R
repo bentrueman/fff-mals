a611311a4cfcb649aa526917676460821ed7eb24
bentrueman <benjamin.trueman@dal.ca>
2022-09-16 10:33:47 -0300

    edits after peer review (2nd batch)

--- R/functions.R
+++ R/functions.R
@@ -1,4 +1,6 @@
 
+# this script is called by source() from paper.Rmd, after loading tidyverse
+
 # this is the opposite of fill()
 
 unfill_vec <- function(x, placeholder = "") { 
@@ -11,5 +13,49 @@ to_list <- function(x) {
     str_replace("(\\d+)([aA-zZ]+)", "^\\1^\\2") %>% 
     unique() %>% 
     glue::glue_collapse(sep = ", ", last = " and ")
+}
+
+# calculate number of matches to a regex in references, as a percentage of total references:
+
+percent_bib_matches <- function(regex) {
+  
+  bib <- read_lines(here::here("Rmarkdown/references.bib")) %>% 
+    tibble(bib = .)
+  rmd <- read_file(here::here("Rmarkdown/paper.Rmd"))
+  
+  keys <- rmd %>% 
+    str_extract_all("(?<=[^\\\\]@)[^;\\]]+") %>% 
+    unlist() %>% 
+    tibble(key = .) %>% 
+    filter(!str_detect(key, "dal.ca")) %>% # email address
+    distinct()
+  
+  filter_bib <- function(x) {
+    bib %>% 
+      filter(
+        str_detect(
+          bib, 
+          glue::glue("@[aA-zZ]+\\{`x`,|author", .open = "`", .close = "`")
+        )
+      ) %>% 
+      filter(str_detect(lag(bib), "@[aA-zZ]"))
+  }
+  
+  sc <- keys %>% 
+    mutate(
+      extr = map(
+        key, 
+        filter_bib
+      )
+    ) %>% 
+    unnest(extr) %>% 
+    distinct() %>% 
+    pull(bib) %>% 
+    str_detect(regex) %>%
+    mean() %>% 
+    round(2)
+  
+  paste0(100 * sc, "% of references matching")
+  
 }
 
